308956222021e17b5dc065516e364549549dc01e
jbautist <jbautist@outlook.es>
2022-04-06 16:34:42 -0300

    Mejoras generales

--- dados_ascii.py
+++ dados_ascii.py
@@ -1,190 +1,118 @@
 dados_ascii = {
     1: '''
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMWKkdlllllllllllllllllllllllllllllllllodkXWMMMMMMMMM
-MMMMMMMWO:.                                      .c0WMMMMMMM
-MMMMMMWx.                                          .kWMMMMMM
-MMMMMMK,                                            ;XMMMMMM
-MMMMMMO'                                            '0MMMMMM
-MMMMMM0'                                            ,KMMMMMM
-MMMMMM0'                                            ,KMMMMMM
-MMMMMM0'                                            ,KMMMMMM
-MMMMMM0'                                            ,KMMMMMM
-MMMMMM0'                  'coddoc.                  ,KMMMMMM
-MMMMMM0'                .oXMMMMMMXl.                ,KMMMMMM
-MMMMMM0'                :NMMMMMMMMX;                ,KMMMMMM
-MMMMMM0'                ,KMMMMMMMM0'                ,KMMMMMM
-MMMMMM0'                 ,xKNWWNKd'                 ,KMMMMMM
-MMMMMM0'                   .',,'.                   ,KMMMMMM
-MMMMMM0'                                            ,KMMMMMM
-MMMMMMO'                                            ,KMMMMMM
-MMMMMMO'                                            ,KMMMMMM
-MMMMMM0'                                            ,0MMMMMM
-MMMMMM0'                                            ,KMMMMMM
-MMMMMMX:                                            cNMMMMMM
-MMMMMMM0;                                          :KMMMMMMM
-MMMMMMMMNkc'....................................,cONMMMMMMMM
-MMMMMMMMMMMNK000000000000000000000000000000000KXNMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+      ,%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#.            
+  #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@         
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(       
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@       
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@       
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@       
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@       
+ @@@@@@@@@@@@@@@@@@@@@%#&@@@@@@@@@@@@@@@@@@@@@       
+ @@@@@@@@@@@@@@@@@@        *@@@@@@@@@@@@@@@@@@       
+ @@@@@@@@@@@@@@@@@*         @@@@@@@@@@@@@@@@@@       
+ @@@@@@@@@@@@@@@@@@        /@@@@@@@@@@@@@@@@@@       
+ @@@@@@@@@@@@@@@@@@@@@@%@@@@@@@@@@@@@@@@@@@@@@       
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@       
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@       
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@       
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@       
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(       
+  (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.        
+      ./(((((((((((((((((((((((((((((((*.
 ''',
 2: '''
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMWX0kkkkkkkkkkxxxxxxxxxkkxxkxxkkxkxxk0XWMMMMMMMMMM
-MMMMMMMMMNk:.                                   .:kNMMMMMMMM
-MMMMMMMMNl.                                       .lXMMMMMMM
-MMMMMMMMk.                                         .kMMMMMMM
-MMMMMMMMx.                           'okOkd:.       dMMMMMMM
-MMMMMMMMx.                          cXMMMMMWx.      dMMMMMMM
-MMMMMMMMx.                         .kMMMMMMMX;      dMMMMMMM
-MMMMMMMMx.                          :KMMMMMNd.      dMMMMMMM
-MMMMMMMMx.                           .cdxxo,        dMMMMMMM
-MMMMMMMMx.                                          dMMMMMMM
-MMMMMMMMx.                                          dMMMMMMM
-MMMMMMMMx.                                          dMMMMMMM
-MMMMMMMMx.                                          dMMMMMMM
-MMMMMMMMx.         ...                              dMMMMMMM
-MMMMMMMMx.      .o0XXKkc.                           dMMMMMMM
-MMMMMMMMx.     .OMMMMMMWd.                          dMMMMMMM
-MMMMMMMMx.     ,KMMMMMMMk.                          dMMMMMMM
-MMMMMMMMd.      cKWMMMNk,                           dMMMMMMM
-MMMMMMMMx.       .;clc,.                            dMMMMMMM
-MMMMMMMM0'                                         .OMMMMMMM
-MMMMMMMMWk,                                       'kWMMMMMMM
-MMMMMMMMMMXxc,'...............................',cxXMMMMMMMMM
-MMMMMMMMMMMMMWNNXXXXXXXXXXXXXXXXXXXXXXXXXNXXXNNWMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+    /@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.          
+  &@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@/        
+ .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@        
+ ,@@@@@@@@@@@@@@@@@@@@@@@@@@@       @@@@@@@@        
+ ,@@@@@@@@@@@@@@@@@@@@@@@@@@         @@@@@@@        
+ ,@@@@@@@@@@@@@@@@@@@@@@@@@@/        @@@@@@@        
+ ,@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%(#@@@@@@@@@@        
+ ,@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@        
+ ,@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@        
+ ,@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@        
+ ,@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@        
+ ,@@@@@@@.       @@@@@@@@@@@@@@@@@@@@@@@@@@@        
+ ,@@@@@@&         @@@@@@@@@@@@@@@@@@@@@@@@@@        
+ ,@@@@@@@.       @@@@@@@@@@@@@@@@@@@@@@@@@@@        
+ .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@        
+  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&        
+    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 ''',
 3: '''
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMWXK00000000000000000000000000000000KXWMMMMMMMMMMM
-MMMMMMMMMKd;..                                ..;oKMMMMMMMMM
-MMMMMMMWk'                                        .kWMMMMMMM
-MMMMMMM0,                                          '0MMMMMMM
-MMMMMMMk.                            .cool;.       .kMMMMMMM
-MMMMMMMk.                          .lXWMMMW0;      .kMMMMMMM
-MMMMMMMk.                          ;XMMMMMMMO.     .kMMMMMMM
-MMMMMMMk.                          'OMMMMMMWd.     .kMMMMMMM
-MMMMMMMk.                           .o0XNKOl.      .kMMMMMMM
-MMMMMMMk.                 .:looo:.    ....         .kMMMMMMM
-MMMMMMMk.                :KWMMMMWKc                .kMMMMMMM
-MMMMMMMk.               '0MMMMMMMMK,               .kMMMMMMM
-MMMMMMMk.               .kWMMMMMMWk.               .kMMMMMMM
-MMMMMMMk.                .lOKXXXOl.                .kMMMMMMM
-MMMMMMMk.       .;looc'     ....                   .kMMMMMMM
-MMMMMMMk.      ;0WMMMWXo.                          .kMMMMMMM
-MMMMMMMk.     .kMMMMMMMX:                          .kMMMMMMM
-MMMMMMMk.     .dWMMMMMM0'                          .kMMMMMMM
-MMMMMMMO.      .ckKXX0d'                           .kMMMMMMM
-MMMMMMMO.         ....                             .kMMMMMMM
-MMMMMMMNc                                          cXMMMMMMM
-MMMMMMMMXd'                                      .oXMMMMMMMM
-MMMMMMMMMWXkoccccccccccccccccccccccccccccccccccokXWMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@          
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@        
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&%@@@@@@@@@@@        
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@        %@@@@@@@        
+ @@@@@@@@@@@@@@@@@@@@@@@@@@%         @@@@@@@        
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@(       @@@@@@@@        
+ @@@@@@@@@@@@@@@@@@@@%/%@@@@@@@@@@@@@@@@@@@@        
+ @@@@@@@@@@@@@@@@@.       ,@@@@@@@@@@@@@@@@@        
+ @@@@@@@@@@@@@@@@@         @@@@@@@@@@@@@@@@@        
+ @@@@@@@@@@@@@@@@@@       @@@@@@@@@@@@@@@@@@        
+ @@@@@@@@@@(,/@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@        
+ @@@@@@@.        @@@@@@@@@@@@@@@@@@@@@@@@@@@        
+ @@@@@@@         @@@@@@@@@@@@@@@@@@@@@@@@@@@        
+ @@@@@@@@       @@@@@@@@@@@@@@@@@@@@@@@@@@@@        
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@        
+ (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.        
+   ,@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.
 ''',
 4: '''
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMWXKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKXWMMMMMMMMMMM
-MMMMMMMMW0o;....................................,cONMMMMMMMM
-MMMMMMMNo.                                         :KMMMMMMM
-MMMMMMWd.                                           cNMMMMMM
-MMMMMMNc        .,:c:,.               ':cc;.        ,KMMMMMM
-MMMMMMN:       ;ONMMMW0:            'kNMMMWKl.      '0MMMMMM
-MMMMMMN:      ,KMMMMMMMK,          .kMMMMMMMNc      '0MMMMMM
-MMMMMMNc      ,KMMMMMMMK,          .kMMMMMMMNc      '0MMMMMM
-MMMMMMN:       ;kNMMMNO;            'xXWMMW0c.      '0MMMMMM
-MMMMMMN:         ':::'                .;::,.        '0MMMMMM
-MMMMMMN:                                            '0MMMMMM
-MMMMMMN:                                            '0MMMMMM
-MMMMMMN:                                            '0MMMMMM
-MMMMMMN:                                            '0MMMMMM
-MMMMMMN:         ..'..                ..''.         '0MMMMMM
-MMMMMMN:       'dKNWNKd'            .l0XWNKx;       '0MMMMMM
-MMMMMMN:      'OMMMMMMM0'          .xWMMMMMMX:      '0MMMMMM
-MMMMMMNc      ;KMMMMMMMX;          .OMMMMMMMWl      '0MMMMMM
-MMMMMMNc      .lXWMMMMXl.           ;0WMMMMNd.      ,0MMMMMM
-MMMMMMNc        .codoc.              .:odol,        ,KMMMMMM
-MMMMMMWo                                            :XMMMMMM
-MMMMMMMXc                                          ,0MMMMMMM
-MMMMMMMMNk:.                                    .;dXMMMMMMMM
-MMMMMMMMMMWX0kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkOKWMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+   ,@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*         
+  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@        
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@       
+ @@@@@@@@(       &@@@@@@@@@@@&       *@@@@@@@@       
+ @@@@@@@&         @@@@@@@@@@@         #@@@@@@@       
+ @@@@@@@@        .@@@@@@@@@@@.        @@@@@@@@       
+ @@@@@@@@@@&*./@@@@@@@@@@@@@@@@@(.*&@@@@@@@@@@       
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@       
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@       
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@       
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@       
+ @@@@@@@@@       @@@@@@@@@@@@@       @@@@@@@@@       
+ @@@@@@@@         @@@@@@@@@@@         @@@@@@@@       
+ @@@@@@@@         @@@@@@@@@@@         @@@@@@@@       
+ @@@@@@@@@@     @@@@@@@@@@@@@@@     @@@@@@@@@@       
+ #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%       
+  *@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(        
+     .&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&.
 ''',
 5: '''
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWMMMMMMMMMMMM
-MMMMMMMMMWKdc;,...............................,;cxXMMMMMMMMM
-MMMMMMMMXl.                                       'xNMMMMMMM
-MMMMMMMNc                                          .xWMMMMMM
-MMMMMMM0'        .',,.                .',,.         :NMMMMMM
-MMMMMMM0'      .l0NWWXO:            .oKNWWXk;       :NMMMMMM
-MMMMMMM0'      oWMMMMMMNc          .xWMMMMMMX;      :NMMMMMM
-MMMMMMM0'      dWMMMMMMWl          .kMMMMMMMN:      :NMMMMMM
-MMMMMMM0'      .xNMMMMXd.           'kNMMMWKl.      :NMMMMMM
-MMMMMMM0'        ':ll:.     .','.     ,clc;.        :NMMMMMM
-MMMMMMM0'                .cOXWWWXk;                 :NMMMMMM
-MMMMMMM0'                oWMMMMMMMX:                :NMMMMMM
-MMMMMMM0'               .xMMMMMMMMWc                :NMMMMMM
-MMMMMMM0'                'kNMMMMMNd.                :NMMMMMM
-MMMMMMM0'         .''.    .,coolc'     ....         :NMMMMMM
-MMMMMMM0'       :kKNNKx;            .ckKNXKd'       :NMMMMMM
-MMMMMMM0'      lNMMMMMMX:          .dWMMMMMMK,      :NMMMMMM
-MMMMMMM0'     .dMMMMMMMWo          .kMMMMMMMNc      :NMMMMMM
-MMMMMMM0'      'OWMMMMNx.           ;0WMMMMNd.      :NMMMMMM
-MMMMMMM0'       .;lool,              .;ldoc'        :NMMMMMM
-MMMMMMMX;                                           oWMMMMMM
-MMMMMMMMO,                                         :KMMMMMMM
-MMMMMMMMMXd;..                                 ..:kNMMMMMMMM
-MMMMMMMMMMMWX0OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO0XWMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+  ,@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@         
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@        
+ @@@@@@@@@@/  &@@@@@@@@@@@@@@@@*  @@@@@@@@@@(       
+ @@@@@@@#        @@@@@@@@@@@,        @@@@@@@(       
+ @@@@@@@         /@@@@@@@@@@         %@@@@@@(       
+ @@@@@@@@       /@@@@@@@@@@@@       %@@@@@@@(       
+ @@@@@@@@@@@@@@@@@@@@#,/@@@@@@@@@@@@@@@@@@@@(       
+ @@@@@@@@@@@@@@@@@/        @@@@@@@@@@@@@@@@@(       
+ @@@@@@@@@@@@@@@@@         (@@@@@@@@@@@@@@@@(       
+ @@@@@@@@@@@@@@@@@@       /@@@@@@@@@@@@@@@@@(       
+ @@@@@@@@@@&*/@@@@@@@@@@@@@@@@@%*(@@@@@@@@@@(       
+ @@@@@@@%        @@@@@@@@@@@/        @@@@@@@(       
+ @@@@@@@         /@@@@@@@@@@         %@@@@@@(       
+ @@@@@@@@       ,@@@@@@@@@@@@       (@@@@@@@(       
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(       
+ (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@        
+   #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 ''',
 6: '''
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMWXOxdddddddddddddddddddddddddddddddx0NMMMMMMMMMMM
-MMMMMMMMMWOc.                                  .'oXMMMMMMMMM
-MMMMMMMMWk.                                       ;KMMMMMMMM
-MMMMMMMMX:       ...        ....         ...       dMMMMMMMM
-MMMMMMMMX;    .lOKXKk:.   ;xKXXX0o'   .oOXXKx;     dMMMMMMMM
-MMMMMMMMX;   .xWMMMMMNo  cNMMMMMMMO' .OMMMMMMNc    dMMMMMMMM
-MMMMMMMMX;   .OMMMMMMWd. oWMMMMMMMK, ,0MMMMMMWo    dMMMMMMMM
-MMMMMMMMX;    ,kNMMWXx.  .oXWMMMNO:   ;ONMMWXd.    dMMMMMMMM
-MMMMMMMMX;      ';:;.      .;:::'.     .'::;.      dMMMMMMMM
-MMMMMMMMX;                                         dMMMMMMMM
-MMMMMMMMX;                                         dMMMMMMMM
-MMMMMMMMX;                                         dMMMMMMMM
-MMMMMMMMX;                                         dMMMMMMMM
-MMMMMMMMX;     .:lol;.     ,cool:.     .:loc,.     dMMMMMMMM
-MMMMMMMMX;    :KWMMMWO,  'kNMMMMWXl. .lXWMMMNk'    dMMMMMMMM
-MMMMMMMMX;   'OMMMMMMMx. oWMMMMMMMK, ,KMMMMMMWo    dMMMMMMMM
-MMMMMMMMX;   .dWMMMMMNc  ;XMMMMMMWk. .xWMMMMMX:    dMMMMMMMM
-MMMMMMMMX;    .;dOOko,    'okO0Ox:.   .:xOOko'     dMMMMMMMM
-MMMMMMMMN:                                        .xMMMMMMMM
-MMMMMMMMMO'                                       cXMMMMMMMM
-MMMMMMMMMMXd,.                                ..:kNMMMMMMMMM
-MMMMMMMMMMMMWK0OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO0XWMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+   /@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@           
+ .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@         
+ @@@@@@@@@&@@@@@@@@@@&@@@@@@@@@@@&@@@@@@@@         
+ @@@@@.       @@@@       ,@@@        @@@@@         
+ @@@@@         @@         @@@        ,@@@@         
+ @@@@@@      ,@@@@.      @@@@@      *@@@@@         
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@         
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@         
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@         
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@         
+ @@@@@@#     @@@@@@     %@@@@@/    .@@@@@@         
+ @@@@@        .@@,        @@@        (@@@@         
+ @@@@@        /@@/        @@@        &@@@@         
+ @@@@@@@.   &@@@@@@&   .@@@@@@@    @@@@@@@         
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@         
+   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#
 '''
 }
--- main.py
+++ main.py
@@ -7,9 +7,9 @@ clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 
 
 print('''
-                      +-----------------------------------------+
-                      |       SIMULADOR DE TIRADA DE DADOS      |
-                      +-----------------------------------------+
+      +-----------------------------------------+
+      |      SIMULADOR DE TIRADA DE DADOS       |
+      +-----------------------------------------+
 ''')
 ENTER = input('Presione ENTER para tirar el dado.')
 clearConsole()
@@ -17,18 +17,13 @@ clearConsole()
 seguir = 'S'
 while seguir == 'S':
     numero = random.randint(1, 6)
-    print(str(numero).rjust(29))
+    print(str(numero).rjust(23))
     print(dados_ascii.dados_ascii[numero])
 
-    # Bucle para comprobar que el usuario ingrese 's, n, S o N.
     while True:  
-        seguir = input('\n¿Quiere tirar el dado nuevamente? [S/N] ')
-        if not seguir.isalpha():
-            print('\nError: has ingresado un caracter incorrecto.')
-        elif seguir.upper() == 'S' or seguir.upper() == 'N':
+        seguir = input('\n¿Quiere tirar el dado nuevamente? [S/N] ').upper()
+        if seguir == 'S' or seguir == 'N':
             break
         else:
-            print('\nError: has ingresado una letra incorrecta.')
-
-    seguir = seguir.upper()
+            print('\nERROR: ha ingresado un carácter invalido.')      
     clearConsole()
